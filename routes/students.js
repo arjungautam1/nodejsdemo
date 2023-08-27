514dc1cc63cc79a1a1f5a47fae6146fec818b646
arjungautam1 <arjungautam5431@gmail.com>
2023-08-27 12:27:33 +0545

    some changes on error handling

--- routes/students.js
+++ routes/students.js
@@ -3,27 +3,6 @@ const router = express.Router()
 const Student= require('../models/student')
 const mongoose= require('mongoose')
 
-router.get('/', async(req, res)=>{
-
-    try{
-        const students= await Student.find()
-        res.json(students)
-
-    }catch(err){
-        res.send('Error'+err)
-    }
-})
-
-router.get('/:id', async(req, res)=>{
-
-    try{
-        const students= await Student.findById(req.params.id)
-        res.json(students)
-
-    }catch(err){
-        res.status(400).json({ error: err.message });
-    }
-})
 
 router.post('/', async (req, res) => {
     const student = new Student({
@@ -36,8 +15,30 @@ router.post('/', async (req, res) => {
         const savedStudent = await student.save();
         res.json(savedStudent);
     } catch (err) {
-         res.status(400).json({ error: err.message }); // Sending error message
-        // res.send("Error"+err)
+        res.status(400).json({ error: "Unable to create student: " + err.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (err) {
+        res.status(500).json({ error: "An error occurred while fetching student data: " + err.message });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const student = await Student.findById(req.params.id);
+
+        if (!student) {
+            return res.status(404).json({ error: "Student not found" });
+        }
+
+        res.json(student);
+    } catch (err) {
+        res.status(500).json({ error: "An error occurred while processing the request" });
     }
 });
 
@@ -47,8 +48,8 @@ router.put('/:id', async (req, res) => {
 
         // Validate studentId to ensure it's a valid ObjectId
         if (!mongoose.Types.ObjectId.isValid(studentId)) {
-            return res.status(404).json({ error: "Student not found" });
-        }
+            return res.status(400).json({ error: "Invalid student ID provided" });
+        } 
 
         const student = await Student.findById(studentId);
 
@@ -71,9 +72,30 @@ router.put('/:id', async (req, res) => {
         const updatedStudent = await student.save();
         res.json(updatedStudent);
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(500).json({ error: "An error occurred while processing the update: " + err.message });
     }
 });
 
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const studentId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(studentId)) {
+            return res.status(400).json({ error: "Invalid student ID format" });
+        }
+
+        const deletedStudent = await Student.findByIdAndRemove(studentId);
+        
+        if (!deletedStudent) {
+            return res.status(404).json({ error: "Student not found for deletion" });
+        }
+
+        res.send("Student with ID " + studentId + " has been deleted.");
+
+    } catch (err) {
+        res.status(500).json({ error: "An error occurred while processing the request" });
+    }
+});
 
 module.exports=router
